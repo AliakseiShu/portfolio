9d97a8dd61520bbf443267270aa0d51b39bf387f
Alex <aliakseishulha2021@gmail.com>
2022-08-14 10:59:02 +0300

    fixed main

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -8,14 +8,11 @@ export const Main = () => {
             <div className={styleContainer.container}>
                 <div className={style.text}>
                     <h3>hi there!</h3>
-                    {/* <Title firstName={"I\'M"} secondName={"ALIAKSEI SHULHA"}/>*/}
-                    {/*<h1>I'M ALIAKSEI SHULHA </h1>*/}
                     <h1 className={style.staticHeadline}>i'm</h1>
                     <span className={style.dynamicHeadline} >
                      <b className={style.isHidden}>aliaksei shulha</b>
                      <b>frontend developer</b>
                      </span>
-                  {/*  <p>FRONTEND DEVELOPER</p>*/}
                 </div>
                 <div className={style.photo}></div>
             </div>
