b214e97756d03d2561e73751163310ecb041b51f
Alex <aliakseishulha2021@gmail.com>
2022-08-19 22:20:24 +0300

    fixed

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -7,8 +7,11 @@ export const Main = () => {
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
                 <div className={style.text}>
+
                     <h3>hi there!</h3>
+
                     <h1 className={style.staticHeadline}>i'm</h1>
+
                     <span className={style.dynamicHeadline} >
                      <b className={style.isHidden}>aliaksei shulha</b>
                      <b>frontend developer</b>
