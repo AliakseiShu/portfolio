496538c0cd5f2d8dba011e8562a73d318fe60902
Alex <aliakseishulha2021@gmail.com>
2022-10-23 19:07:52 +0300

    finished component Experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,18 +7,59 @@ import github from '../assets/github.png';
 import tailwind from '../assets/tailwind.png';
 
 export const Experience = () => {
+
+const techs = [
+    {
+        id: 1,
+        imgTech: html,
+        title: 'HTML',
+        style: 'shadow-orange-500',
+    },
+    {
+        id: 2,
+        imgTech: css,
+        title: 'CSS',
+        style: 'shadow-orange-500',
+    },
+    {
+        id: 3,
+        imgTech: javaScript,
+        title: 'JavaScript',
+        style: 'shadow-orange-500',
+    },
+    {
+        id: 4,
+        imgTech: react,
+        title: 'React',
+        style: 'shadow-orange-500',
+    },
+    {
+        id: 5,
+        imgTech: github,
+        title: 'Github',
+        style: 'shadow-orange-500',
+    },
+    {
+        id: 6,
+        imgTech: tailwind,
+        title: 'Tailwind',
+        style: 'shadow-orange-500',
+    },
+]
     return (
-        <div className="">
-            <div>
+        <div className="bg-gradient-to-b from-gray-800 to-black w-full h-screen">
+            <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
                 <div>
-                    <p>Experience</p>
-                    <p>These are technologies I've worked with</p>
+                    <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
+                    <p className="py-6">These are technologies I've worked with</p>
                 </div>
-                <div>
-                    <div>
-                        <img src={html} alt="Experience"/>
-                        <p>HTML</p>
-                    </div>
+                <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+                    {techs.map(({id, imgTech, title, style}) => (
+                        <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounder-lg ${style}`} >
+                            <img src={imgTech} alt="Experience" className="w-20 mx-auto"/>
+                            <p className="mt-4">{title}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
