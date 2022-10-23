2e461666172fbce1f0e0745c7545d66b8bf73077
Alex <aliakseishulha2021@gmail.com>
2022-10-23 20:04:44 +0300

    install react scroll

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export const Contacts = () => {
     return (
-        <div className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
+        <div data-name="contacts" className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
             <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">Contact</p>
                     <p className="py-6">Submit the form below to get in touch with me</p>
                 </div>
                 <div className="flex justify-center items-center">
-                    <form action="" className="flex flex-col w-full md:w-1/2 ">
+                    <form action="https://getform.io/f/c7c9ee3d-098c-4022-a509-cff34fab3bb5" method="POST" className="flex flex-col w-full md:w-1/2 ">
                         <input type="text"
                                name="name"
                                placeholder="Enter your name"
