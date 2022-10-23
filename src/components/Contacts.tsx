df895429c2eb4c05b653d3353236f585e928a44e
Alex <aliakseishulha2021@gmail.com>
2022-10-23 19:41:49 +0300

    create component Contacts

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -2,25 +2,30 @@ import React from 'react';
 
 export const Contacts = () => {
     return (
-        <div>
-            <div>
-                <div>
-                    <p>Contact</p>
-                    <p>Submit the form below to get in touch with me</p>
+        <div className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
+            <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">Contact</p>
+                    <p className="py-6">Submit the form below to get in touch with me</p>
                 </div>
-                <div>
-                    <form action="" className="">
+                <div className="flex justify-center items-center">
+                    <form action="" className="flex flex-col w-full md:w-1/2 ">
                         <input type="text"
                                name="name"
                                placeholder="Enter your name"
-                               className="p-2 bg-transparent border-2 text-white focus:outline-none"/>
+                               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"/>
                         <input type="text"
                                name="email"
                                placeholder="Enter your email"
-                               className="p-2 bg-transparent border-2 text-white focus:outline-none"/>
-                        <textarea name="message" rows={10}
+                               className=" my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"/>
+                        <textarea name="message"
+                                  placeholder="Enter your massage"
+                                  rows={10}
                                   className="p-2 bg-transparent border-2 rounded-md text-while focus:outline-none"></textarea>
-                        <button className="text-white bg-gradient-to-r from-cyan-500 to-green-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Let's talk</button>
+                        <button
+                            className="text-white bg-gradient-to-r from-cyan-500 to-green-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Let's
+                            talk
+                        </button>
                     </form>
                 </div>
             </div>
