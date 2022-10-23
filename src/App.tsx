a165a45ce33040e65ae2ffce4712496a07e8f186
Alex <aliakseishulha2021@gmail.com>
2022-10-23 12:10:27 +0300

    fixed NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {NavBar} from "./components/NavBar";
+import {Home} from "./components/Home";
 
 function App() {
     return (
         <div>
-            <NavBar />
-
+            <NavBar/>
+            <Home/>
         </div>
     );
 }
+
 export default App;
